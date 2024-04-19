cbabfd8c0047d7b7767faa75eed5f5a9c0e04d42
Erich Armijo <eothandlock@gmail.com>
2024-04-19 02:01:16 -0400

    se Borra componente NavBar de App.jsx y se agrega a HomePage.jsx

--- EduConnect-FrontEnd/src/App.jsx
+++ EduConnect-FrontEnd/src/App.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
 import HomePage from './views/HomePage'
 import Login from './views/login.jsx'
 
 function App () {
   return (
     <>
-      <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/login' element={<Login />} />
