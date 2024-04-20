0291712ba20b8d869f81559f2350ef8e525fcaea
ABJ04 <abj4058@gmail.com>
2024-04-20 13:13:09 -0400

    se soluciona error

--- EduConnect-FrontEnd/src/components/NavBar.jsx
+++ EduConnect-FrontEnd/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 import { Link, useLocation } from 'react-router-dom'
+import { IconLogin2 } from '@tabler/icons-react'
 import '../styles/homeStyle.css'
 
 function NavBar () {
@@ -11,7 +12,6 @@ function NavBar () {
     return null
   }
 
->>>>>>>>> Temporary merge branch 2
   return (
     <Navbar className='bgcolor' expand="lg">
       <Container>
@@ -33,7 +33,7 @@ function NavBar () {
             </Nav>
           </div>
           <Nav>
-            <NavDropdown.Item className='navbar-color' as={Link} to="/login">Login <img src="https://cdn.hugeicons.com/icons/login-03-stroke-rounded.svg" alt="login-03" width="24" height="24" /></NavDropdown.Item>
+            <NavDropdown.Item className='navbar-color' as={Link} to="/login">Login <IconLogin2 stroke={2} /></NavDropdown.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
