cbbb26402553d1be6b197880799ea3130a79fab5
Constanza Valenzuela <kokony.vm@gmail.com>
2024-04-20 22:53:23 -0400

    Se modifica formulario y alerta

--- EduConnect-FrontEnd/src/components/FormularioAdmin.jsx
+++ EduConnect-FrontEnd/src/components/FormularioAdmin.jsx
@@ -1,45 +1,47 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
+import useMostrarAlertaConfirmacion from '../hooks/useMostrarAlertaConfirmacion'
 import '../styles/formulario.css'
+
 function FormularioAdmin () {
   return (
         <>
         <Card className=" formulario p-4">
-            <Form>
-                <Form.Group controlId="formGridEmail">
+            <Form onSubmit={useMostrarAlertaConfirmacion}>
+                <Form.Group>
                     <Form.Label>Nombre</Form.Label>
                     <Form.Control type="string" placeholder="Nombre" />
                 </Form.Group>
-                <Form.Group controlId="formGridEmail">
+                <Form.Group>
                     <Form.Label>Apellido Paterno</Form.Label>
                     <Form.Control type="string" placeholder="Apellido Paterno" />
                 </Form.Group>
-                <Form.Group controlId="formGridEmail">
+                <Form.Group>
                     <Form.Label>Apellido Materno</Form.Label>
                     <Form.Control type="string" placeholder="APellido Materno" />
                 </Form.Group>
-                <Form.Group controlId="formGridPassword">
+                <Form.Group>
                     <Form.Label>Rut</Form.Label>
                     <Form.Control type="string" placeholder="Rut" />
                 </Form.Group>
-                <Form.Group controlId="formGridEmail">
+                <Form.Group>
                     <Form.Label>Dirección</Form.Label>
                     <Form.Control type="string" placeholder="Dirección" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Group>
                     <Form.Label>Comuna</Form.Label>
                     <Form.Control type="string" placeholder="Comuna" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Group>
                     <Form.Label>Teléfono</Form.Label>
                     <Form.Control type="string" placeholder="Teléfono" />
                 </Form.Group>
-                <Form.Group controlId="formGridEmail">
+                <Form.Group>
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="string" placeholder="Email" />
                 </Form.Group>
-                <Form.Group controlId="formGridEmail">
+                <Form.Group>
                     <Form.Label>Clave</Form.Label>
                     <Form.Control type="string" placeholder="Clave" />
                 </Form.Group>
--- EduConnect-FrontEnd/src/components/FormularioColegio.jsx
+++ EduConnect-FrontEnd/src/components/FormularioColegio.jsx
@@ -1,31 +1,32 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
+import useMostrarAlertaConfirmacion from '../hooks/useMostrarAlertaConfirmacion'
 import '../styles/formulario.css'
 function FormularioColegio () {
   return (
         <>
         <Card className=" formulario p-4">
-            <Form>
-                <Form.Group controlId="formGridEmail">
-                <Form.Label>Nombre Establecimiento</Form.Label>
-                <Form.Control type="string" placeholder="Establecimiento" />
+            <Form onSubmit={useMostrarAlertaConfirmacion}>
+                <Form.Group>
+                    <Form.Label>Nombre Establecimiento</Form.Label>
+                    <Form.Control type="string" placeholder="Establecimiento" />
                 </Form.Group>
-                <Form.Group controlId="formGridPassword">
-                <Form.Label>Dirección</Form.Label>
-                <Form.Control type="string" placeholder="Dirección" />
+                <Form.Group>
+                    <Form.Label>Dirección</Form.Label>
+                    <Form.Control type="string" placeholder="Dirección" />
                 </Form.Group>
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Teléfono</Form.Label>
-                <Form.Control type="string" placeholder="Teléfono" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Comuna</Form.Label>
-                <Form.Control type="string" placeholder="Comuna" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Ingresar
-            </Button>
+                <Form.Group>
+                    <Form.Label>Teléfono</Form.Label>
+                    <Form.Control type="string" placeholder="Teléfono" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Comuna</Form.Label>
+                    <Form.Control type="string" placeholder="Comuna" />
+                </Form.Group>
+                 <Button variant="primary" type="submit">
+                     Ingresar
+                </Button>
             </Form>
         </Card>
         </>
