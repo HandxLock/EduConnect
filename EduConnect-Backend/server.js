6cd9a2f85aeb15eb7c56a7c425c7dfe7b4f7133f
Erich Armijo <eothandlock@gmail.com>
2024-04-24 01:58:06 -0400

    style(lint): :art: se mejora sintaxis para cumplir con el estándar de ESLint

--- EduConnect-Backend/server.js
+++ EduConnect-Backend/server.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import { logger } from 'logger-express';
-import 'dotenv/config';
-import loginRoutes from './routes/login.routes.js';
-import personRoutes from './routes/person.routes.js';
+import express from 'express'
+import cors from 'cors'
+import { logger } from 'logger-express'
+import 'dotenv/config'
+import loginRoutes from './routes/login.routes.js'
+import personRoutes from './routes/person.routes.js'
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(logger());
+app.use(express.json())
+app.use(cors())
+app.use(logger())
 
-app.use(personRoutes);
-app.use(loginRoutes);
+app.use(personRoutes)
+app.use(loginRoutes)
 
-app.listen(PORT, console.log(`Server on http://localhost:${PORT}`));
+app.listen(PORT, console.log(`Server on http://localhost:${PORT}`))
