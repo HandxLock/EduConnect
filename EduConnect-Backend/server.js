4a8da8ce63a1c07bdadd8de33d48546b3d42e781
Erich Armijo <eothandlock@gmail.com>
2024-04-27 02:27:50 -0400

    chore: :art: se agrega ruta superAdmin

--- EduConnect-Backend/server.js
+++ EduConnect-Backend/server.js
@@ -2,9 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import { logger } from 'logger-express'
 import 'dotenv/config'
+import superAdminRoutes from './routes/superAdmin.routes.js'
 import loginRoutes from './routes/login.routes.js'
 import personRoutes from './routes/person.routes.js'
-
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cors())
 app.use(logger())
 
+app.use(superAdminRoutes)
 app.use(personRoutes)
 app.use(loginRoutes)
-
 app.listen(PORT, console.log(`Server on http://localhost:${PORT}`))
