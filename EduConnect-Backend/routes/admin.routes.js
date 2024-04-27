c774e721d161a12f39897705805199569fd0e7a1
ABJ04 <abj4058@gmail.com>
2024-04-27 15:58:59 -0400

    feat: :sparkles: se añaden rutas
    se crea rutas (asignaturas) con sus respectuvas funciones

--- EduConnect-Backend/routes/admin.routes.js
+++ EduConnect-Backend/routes/admin.routes.js
@@ -1,9 +1,13 @@
-import { Router } from 'express'
-import loginPerson from '../src/controllers/loginController.js'
-import validateParamsLogin from '../middlewares/valid.params.login.js'
+import express from 'express'
+import { createAsignatura, getAsignaturaById, getAllAsignaturas, updateAsignatura, deleteAsignatura } from '../src/controllers/AdminController'
 
-const router = Router()
+const router = express.Router()
 
-router.post('/login', validateParamsLogin, loginPerson)
+// Rutas Asignaturas
+router.post('/asignaturas', createAsignatura)
+router.get('/asignaturas/:asignatura_id', getAsignaturaById)
+router.get('/asignaturas', getAllAsignaturas)
+router.put('/asignaturas/:asignatura_id', updateAsignatura)
+router.delete('/asignaturas/:asignatura_id', deleteAsignatura)
 
 export default router
