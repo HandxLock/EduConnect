ff3ff0a8a8c0fbf6ef0851d0950759da7d6f3b72
Claudio-GR <claudio.gonzalez.rivera@gmail.com>
2024-04-29 02:04:09 -0400

    fix: :bug: Arreglando bugs en la ruta get de usuarios
    Se corrige controller y ruta para que funciones bien la verificación del JWT con la contraseña encriptada

--- EduConnect-Backend/routes/user.routes.js
+++ EduConnect-Backend/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { createNewUser, getPersonByID } from '../src/controllers/userController.js'
+import { createNewUser, getUserByEmail } from '../src/controllers/userController.js'
 import validateParamsUser from '../middlewares/valid.params.user.js'
 import validateLogin from '../middlewares/valid.login.js'
 
 const router = Router()
 
 router.post('/user', validateParamsUser, createNewUser)
-router.get('/user/:email', validateLogin, getPersonByID)
+router.get('/user/:email', validateLogin, getUserByEmail)
 
 export default router
