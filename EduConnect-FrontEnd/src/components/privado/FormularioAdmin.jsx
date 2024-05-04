bac00396d7ab6183b6391bf37114fa646978fc8f
Erich Armijo <eothandlock@gmail.com>
2024-05-03 23:31:57 -0400

    feat: :sparkles: se modifican los archivos para ahora mostrar los datos del backend

--- EduConnect-FrontEnd/src/components/privado/FormularioAdmin.jsx
+++ EduConnect-FrontEnd/src/components/privado/FormularioAdmin.jsx
@@ -1,56 +1,78 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
-import useMostrarAlertaConfirmacion from '../../hooks/useMostrarAlertaConfirmacion'
+// import useMostrarAlertaConfirmacion from '../../hooks/useMostrarAlertaConfirmacion'
 import '../../styles/privado/formulario.css'
+import { useState } from 'react'
+import { agregarAdmin } from '../../../api/auth.js'
 
 function FormularioAdmin () {
+  const [rut, setRut] = useState('')
+  const [nombre, setNombre] = useState('')
+  const [apellidoPaterno, setApellidoPaterno] = useState('')
+  const [apellidoMaterno, setApellidoMaterno] = useState('')
+  const [email, setEmail] = useState('')
+  const [clave, setClave] = useState('')
+  const [direccion, setDireccion] = useState('')
+  const [telefono, setTelefono] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await agregarAdmin(rut, nombre, apellidoPaterno, apellidoMaterno, email, clave, direccion, telefono)
+      setSubmitted(true)
+    } catch (error) {
+      console.error('Error al agregar administrador:', error)
+      // Aquí puedes manejar el error, mostrar una alerta, etc.
+    }
+  }
+  if (submitted) {
+    window.location.href = '/superadmin'
+    // Cambi}a este valor para ajustar el tiempo de espera antes de la redirección
+  }
   return (
         <>
-        <Card className=" formulario">
-            <Form onSubmit={useMostrarAlertaConfirmacion}>
-                <Form.Group>
-                    <Form.Label>Nombre</Form.Label>
-                    <Form.Control type="string" placeholder="Nombre" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Apellido Paterno</Form.Label>
-                    <Form.Control type="string" placeholder="Apellido Paterno" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Apellido Materno</Form.Label>
-                    <Form.Control type="string" placeholder="APellido Materno" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Rut</Form.Label>
-                    <Form.Control type="string" placeholder="Rut" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Dirección</Form.Label>
-                    <Form.Control type="string" placeholder="Dirección" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Comuna</Form.Label>
-                    <Form.Control type="string" placeholder="Comuna" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Teléfono</Form.Label>
-                    <Form.Control type="string" placeholder="Teléfono" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="string" placeholder="Email" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Clave</Form.Label>
-                    <Form.Control type="string" placeholder="Clave" />
-                </Form.Group>
-                <Button variant="primary" type="submit" className='m-3'>
-                    Ingresar
-                </Button>
-            </Form>
-        </Card>
-        </>
+      <Card className="formulario">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Rut</Form.Label>
+            <Form.Control type="string" placeholder="Rut" value={rut} onChange={(e) => setRut(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="string" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Apellido Paterno</Form.Label>
+            <Form.Control type="string" placeholder="Apellido Paterno" value={apellidoPaterno} onChange={(e) => setApellidoPaterno(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Apellido Materno</Form.Label>
+            <Form.Control type="string" placeholder="Apellido Materno" value={apellidoMaterno} onChange={(e) => setApellidoMaterno(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="string" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Clave</Form.Label>
+            <Form.Control type="string" placeholder="Clave" value={clave} onChange={(e) => setClave(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Dirección</Form.Label>
+            <Form.Control type="string" placeholder="Dirección" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Teléfono</Form.Label>
+            <Form.Control type="string" placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="m-3">
+            Ingresar
+          </Button>
+        </Form>
+      </Card>
+    </>
   )
 }
 
--- EduConnect-FrontEnd/src/components/privado/FormularioColegio.jsx
+++ EduConnect-FrontEnd/src/components/privado/FormularioColegio.jsx
@@ -1,34 +1,66 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
-import useMostrarAlertaConfirmacion from '../../hooks/useMostrarAlertaConfirmacion'
+// import useMostrarAlertaConfirmacion from '../../hooks/useMostrarAlertaConfirmacion'
 import '../../styles/privado/formulario.css'
+import { agregarColegio } from '../../../api/auth.js'
+import { useState } from 'react'
 function FormularioColegio () {
+  const [nombre, setNombre] = useState('')
+  const [descripcion, setDescripcion] = useState('')
+  const [rut, setRut] = useState('')
+  const [direccion, setDireccion] = useState('')
+  const [telefono, setTelefono] = useState('')
+  const [email, setEmail] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await agregarColegio(nombre, descripcion, rut, telefono, email)
+      // Marca el formulario como enviado
+      setSubmitted(true)
+    } catch (error) {
+      console.error('Error al agregar colegio:', error)
+      // Aquí puedes manejar el error, mostrar una alerta, etc.
+    }
+  }
+  if (submitted) {
+    window.location.href = '/superadmin'
+  }
   return (
         <>
-        <Card className=" formulario">
-            <Form onSubmit={useMostrarAlertaConfirmacion}>
-                <Form.Group>
-                    <Form.Label>Nombre Establecimiento</Form.Label>
-                    <Form.Control type="string" placeholder="Establecimiento" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Dirección</Form.Label>
-                    <Form.Control type="string" placeholder="Dirección" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Teléfono</Form.Label>
-                    <Form.Control type="string" placeholder="Teléfono" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Comuna</Form.Label>
-                    <Form.Control type="string" placeholder="Comuna" />
-                </Form.Group>
-                 <Button variant="primary" type="submit">
-                     Ingresar
-                </Button>
-            </Form>
-        </Card>
+        <Card className="formulario">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Nombre Establecimiento</Form.Label>
+            <Form.Control type="string" placeholder="Establecimiento" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Descripción</Form.Label>
+            <Form.Control type="string" placeholder="Descripción" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>RUT</Form.Label>
+            <Form.Control type="string" placeholder="RUT" value={rut} onChange={(e) => setRut(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Dirección</Form.Label>
+            <Form.Control type="string" placeholder="Dirección" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Teléfono</Form.Label>
+            <Form.Control type="string" placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="string" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Ingresar
+          </Button>
+        </Form>
+      </Card>
         </>
   )
 }
