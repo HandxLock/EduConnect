365a55f69b89c3c3252624f98e53350a5471bff4
Erich Armijo <eothandlock@gmail.com>
2024-05-03 23:29:35 -0400

    feat: :sparkles: se crean las rutas protegidas de superAdmin y admin

---
+++ EduConnect-FrontEnd/src/ProtectedRouteAdmin.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from './context/AuthContext'
+function ProtectedRouteAdmin () {
+  const { loading, isAuthenticated } = useAuth()
+  console.log(loading, isAuthenticated)
+  if (loading) return <h1>Cargando</h1>
+  if (!loading && !isAuthenticated) return <Navigate to='/login' replace />
+  return <Outlet />
+}
+export default ProtectedRouteAdmin
