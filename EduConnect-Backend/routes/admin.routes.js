0ba0944430976ccfc1bf788fec824e33d65028ae
Claudio Gonzalez <claudio.gonzalez.rivera@gmail.com>
2024-05-05 01:36:17 -0400

    feat: :sparkles: agregar controllers para docentes y alumnos RUD

--- EduConnect-Backend/routes/admin.routes.js
+++ EduConnect-Backend/routes/admin.routes.js
@@ -2,16 +2,24 @@
 
 import { Router } from 'express'
 import validateParamsAlumno from '../middlewares/valid.params.Alumno.js'
-import { createAsignatura, getAsignaturaById, getAllAsignaturas, updateAsignatura, deleteAsignatura, createCurso, getCursoById, getCurso, updateCurso, deleteCurso, createNewAlumno, createNewDocente } from '../src/controllers/AdminController.js'
+import { createAsignatura, getAsignaturaById, getAllAsignaturas, updateAsignatura, deleteAsignatura, createCurso, getCursoById, getCurso, updateCurso, deleteCurso, createNewAlumno, createNewDocente, getDocentesController, deleteDocenteController, updateDocenteController, getAlumnosController, updateAlumnoController, deleteAlumnoController } from '../src/controllers/AdminController.js'
 import validateParamsDocente from '../middlewares/valid.params.Docente.js'
 
 const router = Router()
 
 // Rutas Alumnos
 router.post('/admin/alumno', validateParamsAlumno, createNewAlumno)
+router.get('/admin/alumnos', getAlumnosController)
+router.put('/admin/alumnos/:alumno_id', updateAlumnoController)
+router.delete('/admin/alumnos/:alumno_id', deleteAlumnoController)
+
 
 // Rutas Docentes
 router.post('/admin/docente', validateParamsDocente, createNewDocente)
+// router.get('/admin/docentes/:docente_id')
+router.get('/admin/docentes', getDocentesController)
+router.put('/admin/docentes/:docente_id', updateDocenteController)
+router.delete('/admin/docentes/:docente_id', deleteDocenteController)
 
 // Rutas Asignaturas
 router.post('/admin/asignaturas', createAsignatura)
