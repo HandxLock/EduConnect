34f20b8b4dd703319ebff79ffda7b111e2ebc6f2
Erich Armijo <eothandlock@gmail.com>
2024-05-06 05:12:23 -0400

    feat: se crean variables de entorno

--- EduConnect-FrontEnd/src/services/axios.js
+++ EduConnect-FrontEnd/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000' || 'https://educonnect-f34m.onrender.com/',
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}` || 'http://localhost:3000',
   withCredentials: true
 })
 
