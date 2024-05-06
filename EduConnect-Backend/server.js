aff4a5a4f6171e8089b3fd338ab0687bd9a9f522
Erich Armijo <109211800+HandxLock@users.noreply.github.com>
2024-05-06 04:03:40 -0400

    Update server.js

--- EduConnect-Backend/server.js
+++ EduConnect-Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://educonnectfront.onrender.com',
   credentials: true
 }))
 app.use(logger())
