fafbffd35ed8f28e54925022e2cc48be532ad889
ABJ04 <abj4058@gmail.com>
2024-05-08 19:46:09 -0400

    feat: :sparkles: herpers Errors
    Se añaden Helpers Errors para valids

--- EduConnect-Backend/middlewares/valid.params.user.js
+++ EduConnect-Backend/middlewares/valid.params.user.js
@@ -1,7 +1,10 @@
-const validateParamsUser = (req, res, next) => {
+import sendErrorResponse from '../utils/utils.js'
+
+export const validateParamsUser = (req, res, next) => {
   const { user } = req.body
   if (!user.rut || !user.nombre || !user.apellido1 || !user.apellido2 || !user.email || !user.clave || !user.direccion || !user.telefono || !user.perfilId) {
-    return res.status(400).json({ error: 'debe ingresar todos los campos para registrar una persona' })
+    sendErrorResponse(res, 'error.incon')
+    return
   }
   next()
 }
