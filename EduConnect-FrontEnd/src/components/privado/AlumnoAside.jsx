0d915a7c5376389a38c5c11441ae36646b641ee4
Erich Armijo <eothandlock@gmail.com>
2024-05-09 06:21:20 -0400

    fix: :bug: se arregla codigo

--- EduConnect-FrontEnd/src/components/privado/AlumnoAside.jsx
+++ EduConnect-FrontEnd/src/components/privado/AlumnoAside.jsx
@@ -1,31 +1,30 @@
-import { Col, Container, Row } from 'react-bootstrap'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import Dropdown from 'react-bootstrap/Dropdown'
-import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
 import '../../styles/privado/aside.css'
-
-function AsideAlumno () {
+import { useAuth } from '../../context/AuthContext'
+const AsideAlumno = () => {
+  const { user, logoutContext } = useAuth()
   return (
-    <div className="aside ">
-      <div className='perfil'>
-        <Image src="https://cdn-icons-png.flaticon.com/512/67/67902.png" roundedCircle className='imgperfil' style={{ width: '100px', height: '100px', marginBottom: '10px' }} />
-        <h3>Estudiante</h3>
-        <p>Profile</p>
+    <>
+    <aside className='aside'>
+      <div className='contenedor'>
+        <div className='perfil'>
+          <h4>Bienvenido {user.nombre}</h4>
+        </div>
+        <div className='botones'>
+          <Button variant="secondary" size="lg">
+            Alumno
+          </Button>
+
+        </div>
       </div>
-      <Container fluid>
-        <Row>
-          <Col md={6}>
-            <div className='boton-seleccion'>
-              <DropdownButton id="dropdown-basic-button" title="Selecciona" drop={'down-centered'}>
-                <Dropdown.Item href='/superAdmin/alumno:id/alumnoNotas'>Notas</Dropdown.Item>
-                <Dropdown.Item href="/superAdmin/alumno:id/alumnoObservaciones">Observaciones</Dropdown.Item>
-                <Dropdown.Item href="/superAdmin/alumno:id/alumnoAnotaciones">Anotaciones</Dropdown.Item>
-              </DropdownButton>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+      <div className='cerrarSesion'>
+        <Button variant="danger" size="lg" onClick={() => logoutContext()}>
+            Cerrar Sesion
+        </Button>
+
+      </div>
+    </aside>
+    </>
   )
 }
 
